2178b9750c0094fd8b9e2a941ec4786620c507e8
王永朋 <@LAPTOP-42050GDO>
2018-05-13 20:46:58 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\Gorgeous\\nchome\\freemarker.dat"));
+		HashMap<String, Object> readObject = (HashMap<String, Object>) ois.readObject();
+		ois.close();
+		System.out.println(readObject);
+	}
+
+}
